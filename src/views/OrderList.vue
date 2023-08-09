ad4bce143cd187c55ae0ee3e48d84032c536bc24
zhang-wen-zhong <yoooook123@gmail.com>
2023-08-09 20:15:12 +0800

    會員及電子報基本資料串接

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,45 +1,47 @@
+
 <template>
     <div class="main_content">
-        <h3>訂單管理</h3>
+        <h3>訂單列表</h3>
         <div class="action_container">
             <div class="searchbar">
                 <input type="text" name="search" id="search" placeholder="請輸入關鍵字" />
                 <button class="btn">搜尋</button>
             </div>
         </div>
+
+        
+
         <div class="table_wrap">
             <table>
                 <tr>
-                    <th>
-                        <button>
-                            NO.
-                            <Icon type="md-arrow-dropdown" />
-                        </button>
-                    </th>
+                    <th>會員編號</th>
                     <th>訂單編號</th>
                     <th>票券名稱</th>
-                    <th>會員編號</th>
                     <th>
                         下單時間
                         <button>
                             <Icon type="md-arrow-dropdown" />
                         </button>
                     </th>
-                    <th>總金額
+                    <th>
+                        總金額
                         <button>
                             <Icon type="md-arrow-dropdown" />
                         </button>
                     </th>
                     <th>狀態</th>
                 </tr>
-                <tr v-for="(item, index) in tableData" :key="index">
-                    <td>{{ index + 1 }}</td>
-                    <td><router-link to="/order_content">{{ item.number }}</router-link></td>
-                    <td>{{ item.tickit }}</td>
-                    <td>{{ item.member }}</td>
-                    <td>{{ item.time }}</td>
-                    <td>{{ item.price }}</td>
-                    <td>{{ item.state }}</td>
+                <tr v-for="(item, index) in dataFromMySQL" :key="index">
+
+
+                    <!-- 會員編號 -->
+                    <td>{{ item.ord_mem }}</td>
+                    <!-- 訂單編號 -->
+                    <td>{{ item.ord_id }}</td>
+                    <td>{{ item.ord_receiver }}</td>
+                    <td>{{ item.ord_date }}</td>
+                    <td>{{ item.ord_sum }}</td>
+                    <td>{{ item.ord_status }}</td>
                 </tr>
             </table>
         </div>
@@ -47,16 +49,15 @@
 </template>
   
 <script>
+import {GET} from '@/plugin/axios'
+
 export default {
     data() {
         return {
-            tableData: [
-                { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '取消' },
-                { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '完成' },
-                { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '取消' },
-                { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '完成' },
-                { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '取消' },
-            ],
+            dataFromMySQL:[],
+
+            //     { number: '1234', tickit: '台北101', member: '王小明', time: '2023-08-01', price: '1200元', state: '取消' },
+            
         };
     },
     methods: {
@@ -65,9 +66,19 @@ export default {
         },
         // You can add other methods for handling backend data retrieval, update, etc.
     },
+    mounted() {
+        GET(`${this.$URL}/OrderList.php`)
+            .then((res) => {
+                this.dataFromMySQL = res;
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @import "@/assets/scss/main.scss";
 
 a {
