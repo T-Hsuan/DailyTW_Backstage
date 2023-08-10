ae178997c4a6ea4d8a221d0e69f0bc1fb0b53922
zhang-wen-zhong <yoooook123@gmail.com>
2023-08-10 09:46:39 +0800

    會員資料管理增加搜尋功能

--- public/phpfile/MemberMgt.php
+++ public/phpfile/MemberMgt.php
@@ -1,6 +1,8 @@
 <?php 
 	header('Access-Control-Allow-Origin:*');
 	header("Content-Type:application/json;charset=utf-8");
+
+	
 try {
 	//引入連線工作的檔案
 	require_once("connectDailyTW.php");
@@ -16,4 +18,24 @@ try {
 	echo "錯誤原因 : ", $e->getMessage(), "<br>";
 	//echo "系統暫時不能正常運行，請稍後再試<br>";	
 }
+
+// try{
+//   require_once("../connectBooks.php");
+//   $sql = "select * from member where memId=:memId";
+//   $member = $pdo->prepare($sql);
+//   $member->bindValue(":memId", $_GET["memId"]);
+//   $member->execute();
+  
+//   if( $member->rowCount() === 0 ){ //找不到
+//     //傳回空的JSON字串
+//     echo "{}";
+//   }else{ //找得到
+//     //取回一筆資料
+//     $memRow = $member->fetch(PDO::FETCH_ASSOC);
+//     //送出json字串
+//     echo json_encode($memRow);
+//   }	
+// }catch(PDOException $e){
+//   echo $e->getMessage();
+// }
 ?>
