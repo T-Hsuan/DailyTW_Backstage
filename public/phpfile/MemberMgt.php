51e51c572b9e48dc7ab23a17721e362ad81076a7
zhang-wen-zhong <yoooook123@gmail.com>
2023-08-11 14:23:27 +0800

    會員資料管理功能串接

--- public/phpfile/MemberMgt.php
+++ public/phpfile/MemberMgt.php
@@ -1,41 +1,57 @@
 <?php 
-	header('Access-Control-Allow-Origin:*');
-	header("Content-Type:application/json;charset=utf-8");
+	header('Access-Control-Allow-Origin: *');
+	//設置 CORS（跨源資源共享）
+	header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
+	try {
+        require_once("connectDailyTW.php");
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            // Fetch place data from database
+            $sql = "SELECT * FROM member";
+            $products = $pdo->query($sql);
+            $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($prodRows);
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Update status
+            $data = json_decode(file_get_contents('php://input'), true);
+
+            $memId = $data['mem_id'];
+            $newStatus = $data['new_status'];
+
+            // Update the database
+            $sql = "UPDATE member SET mem_status = :newStatus WHERE mem_id = :memId";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':newStatus', $newStatus);
+            $stmt->bindValue(':memId', $memId);
+            $stmt->execute();
+
+            // Respond with success
+            echo json_encode(["message" => "Status updated successfully"]);
+        } else {
+            http_response_code(400);
+            echo json_encode(["message" => "Invalid request method"]);
+        }
+    } catch (Exception $e) {
+        echo "錯誤行號 : ", $e->getLine(), "<br>";
+	    echo "錯誤原因 : ", $e->getMessage(), "<br>";
+    }	
+// try {
+// 	//引入連線工作的檔案
+// 	require_once("connectDailyTW.php");
 	
-try {
-	//引入連線工作的檔案
-	require_once("connectDailyTW.php");
-	
-	//執行sql指令並取得pdoStatement
-	$sql = "select * from member";
-	$products = $pdo->query($sql); 
-	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($prodRows);
+// 	//執行sql指令並取得pdoStatement
+// 	$sql = "select * from member";
+// 	$products = $pdo->query($sql); 
+// 	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+//   echo json_encode($prodRows);
 
-} catch (Exception $e) {
-	echo "錯誤行號 : ", $e->getLine(), "<br>";
-	echo "錯誤原因 : ", $e->getMessage(), "<br>";
-	//echo "系統暫時不能正常運行，請稍後再試<br>";	
-}
-
-// try{
-//   require_once("../connectBooks.php");
-//   $sql = "select * from member where memId=:memId";
-//   $member = $pdo->prepare($sql);
-//   $member->bindValue(":memId", $_GET["memId"]);
-//   $member->execute();
-  
-//   if( $member->rowCount() === 0 ){ //找不到
-//     //傳回空的JSON字串
-//     echo "{}";
-//   }else{ //找得到
-//     //取回一筆資料
-//     $memRow = $member->fetch(PDO::FETCH_ASSOC);
-//     //送出json字串
-//     echo json_encode($memRow);
-//   }	
-// }catch(PDOException $e){
-//   echo $e->getMessage();
+// } catch (Exception $e) {
+// 	echo "錯誤行號 : ", $e->getLine(), "<br>";
+// 	echo "錯誤原因 : ", $e->getMessage(), "<br>";
+// 	//echo "系統暫時不能正常運行，請稍後再試<br>";	
 // }
+
+
 ?>
--- src/views/MemberMgt.vue
+++ src/views/MemberMgt.vue
@@ -3,13 +3,13 @@
         <h3>會員資料管理</h3>
         <div class="action_container">
             <div class="searchbar">
-                <!-- v-model="searchText" -->
-                <input  
+
+                <input  v-model="searchText"
                 type="text" 
                 name="search" 
                 id="search" 
                 placeholder="請輸入關鍵字" />
-                <button class="btn" @click="applySearch">搜尋</button>
+                <button class="btn" @click="search">搜尋</button>
             </div>
         </div>
         <div class="table_wrap">
@@ -22,13 +22,13 @@
                     <th>帳號狀態</th>
                 </tr>
  
-                <tr v-for="(item, index) in filteredItems" :key="index" >
-                    <td>{{ index + 1 }}</td>
+                <tr v-for="(item, index) in tableData" :key="index" >
+                    <td>{{ item.mem_id }}</td>
                     <td>{{ item.mem_name }}</td>
                     <td>{{ item.mem_phone }}</td>
                     <td>{{ item.mem_email }}</td>
                     <td>
-                        <Switch size="large" v-model="item.status">
+                        <Switch size="large" v-model="item.mem_status" true-value="1" false-value="0">
                             <template #open>
                                 <span>ON</span>
                             </template>
@@ -38,95 +38,84 @@
                         </Switch>
                     </td>
                 </tr>
-                <!-- <Select v-model="selectedRegions[index]">
-                        <Option v-for="region in regionList" :value="region.value" :key="region.value">{{ region.label }}</Option>
-                    </Select> -->
+                
                 
                     
                 </table>
-                <p v-if="filteredItems.length === 0" class="ivu-text-center ivu-m">
-                    * 沒有找到符合搜尋條件的結果
-                </p>
+                
         </div>
         <!-- 切換分頁 -->
         <div class="pages">
-            <Page 
-            :total="page.total" 
-            :current="page.index"
-            :page-size="page.size"
-            @on-change="pIndexChange"
-            />
+            <Page :total="dataLength" v-model="page.index" :page-size="page.size"  />
         </div>
     </div>
 </template>
   
 <script>
-import {GET} from '@/plugin/axios'
+import { GET, POST } from '@/plugin/axios'
 
 export default {
     data() {
         return {
             rawData:[],
-            tableData: [],
+
             page: {
                 index: 1, //當前分頁
                 size: 20, //一頁多少筆資料
-                total: 0,
             },
-            regionList: [
-                {
-                    value: '待修改',
-                    label: '待修改'
-                },
-                {
-                    value: '審核中',
-                    label: '審核中'
-                },
-                {
-                    value: '已發布',
-                    label: '已發布'
-                },
-                
-            ],
-            selectedRegions: [], // Each select's value is stored in this array
+            sortType: '',
+            sortColumn: '',
+            sortIcon: 'md-arrow-dropdown',
             searchText: '',
+            filterText: '',
+        
         };
     },
 
     computed: {
-    // search
-    filteredItems() {
-      if (this.searchText === "") {
-        return this.rawData;
-      }
-
-      return this.rawData.filter((item) =>
-        Object.values(item).some((val) => String(val).includes(this.searchText))
-      );
+        //搜尋結果資料
+        searchData() {
+            return this.rawData.filter(item => 
+                item.mem_name.toLowerCase().includes(this.filterText.toLowerCase()) ||
+                item.mem_id.toString().includes(this.filterText.toLowerCase())
+            )
+        },
+        //資料排序
+        sortData() {
+            const arr= [...this.searchData];
+            return arr.sort((a, b) => {
+                const aValue = a[this.sortColumn];
+                const bValue = b[this.sortColumn];
+                if (this.sortType === 'no') {
+                    return this.sortIcon === 'md-arrow-dropdown' ? aValue - bValue : bValue - aValue;
+                } else if (this.sortType === 'updateDate' || this.sortType === 'stayTime') {
+                    return this.sortIcon === 'md-arrow-dropdown' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+                }
+            });
+        },
+        //資料分頁
+        tableData() {
+            return this.sortData.slice((this.page.index - 1) * this.page.size, this.page.index * this.page.size);
+        },
+        dataLength() {
+            return this.searchData.length;
+        },
     },
-  },
 
     methods: {
         
-        applySearch() {
-            this.searchText = document.getElementById("search").value;
-            this.getHome();
-        },
-        // deleteRow(index) {
-        //     this.tableData.splice(index, 1);
-        // },
-        // You can add other methods for handling backend data retrieval, update, etc.
-        
-        getHome() {
-            const startIdx = (this.page.index - 1) * this.page.size;
-            const endIdx = startIdx + this.page.size;
-
-            this.tableData = this.rawData.slice(startIdx, endIdx);
+        //排序
+        sortBy(column, sortType) {
+            this.sortType = sortType;
+            this.sortColumn = column;
+            this.sortIcon = this.sortIcon === 'md-arrow-dropdown' ? 'md-arrow-dropup' : 'md-arrow-dropdown';
         },
 
-        pIndexChange(i) {
-            this.page.index = i;
-            this.getHome();
+        //搜尋
+        search() {
+            const searchTerm = this.searchText.toLowerCase();
+            this.filterText = searchTerm;
+            this.page.index = 1;
         },
     },
     
@@ -136,8 +125,6 @@ export default {
             .then((res) => {
                 console.log(res);
                 this.rawData = res;
-                this.page.total = this.rawData.length;
-                this.getHome();
             })
             .catch((err) => {
                 console.log(err);
